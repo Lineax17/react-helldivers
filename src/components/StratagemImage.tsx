61101aa551a3490497455d47d2a0507dc40d9ac4
Max Burckhard <max.burckhard@stud-mail.uni-wuerzburg.de>
2024-07-02 00:40:27 +0200

    Trying to add image via function call

--- src/components/StratagemImage.tsx
+++ src/components/StratagemImage.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import StratagemPage from "./StratagemPage";
+import StratagemSublist from "./StratagemSublist";
 
 interface StratagemImageProps {
   imagePath: string;
 }
 
+function StratagemImage(stratagemImageProps:StratagemImageProps) {
+  return <div className="stratagem_image" onClick={() =>"Onclick Image Test"}><img src={stratagemImageProps.imagePath} alt="Stratagem Image" /></div>;
+}
+/*
 const StratagemImage: React.FC<StratagemImageProps> = ({ imagePath }) => {
   return <div className="stratagem_image" onClick={() =>"Onclick Image Test"}><img src={imagePath} alt="Stratagem Image" /></div>;
 };
+
+*/
 
 export default StratagemImage;
--- src/components/StratagemSublist.tsx
+++ src/components/StratagemSublist.tsx
@@ -1,15 +1,41 @@
 import React from "react";
 import StratagemImage from "./StratagemImage";
+import stratagemImage from "./StratagemImage";
+import {Simulate} from "react-dom/test-utils";
+import compositionEnd = Simulate.compositionEnd;
 
 interface StratagemSublistProps{
     sectionName: string
 }
 
+interface StratagemImage{
+    filePath:string
+}
+
+const Components = {
+    stratagemImage:StratagemImage
+};
+
+
+const dynamicComponent = (imagePath:StratagemImage) => {
+    return React.createElement(StratagemImage, {imagePath:"../../media/stratagems/bridge/HMG_Emplacement.svg"});
+
+};
+/*
+{dynamicComponent({filePath:'../../media/stratagems/bridge/HMG_Emplacement.svg'})}
+Components[stratagemImage.filePath], { key: stratagemImage._uid, block: stratagemImage }
+ */
+
 function StratagemSublist(stratagemSublistProps:StratagemSublistProps) {
     return <div>
         <h2>{stratagemSublistProps.sectionName}</h2>
         <StratagemImage imagePath="../../media/stratagems/bridge/HMG_Emplacement.svg"></StratagemImage>
+        {dynamicComponent({filePath:"../../media/stratagems/bridge/HMG_Emplacement.svg"})}
     </div>
 }
+
+/*
+<img src={"../../media/stratagems/bridge/HMG_Emplacement.svg"} alt="Stratagem Image"/>
+ */
 
 export default StratagemSublist;
