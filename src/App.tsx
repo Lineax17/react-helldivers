43af3cb36c4919b538547edb43dc0c6c6294542a
Max Burckhard <max.burckhard@stud-mail.uni-wuerzburg.de>
2024-07-01 22:54:12 +0200

    Test

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import StartButton from "./components/StartButton";
 function App() {
     return <div>
         <StratagemImage imagePath="../../media/stratagems/hangar/Eagle Airstrike.svg"></StratagemImage>
-        <StartButton/>
     </div>
 }
 
