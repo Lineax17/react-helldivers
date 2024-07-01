e46b669818ffd2c93d67a13f3ff08242fde973f9
Max Burckhard <max.burckhard@stud-mail.uni-wuerzburg.de>
2024-07-01 23:51:33 +0200

    Added Basic Structure for Stratagem List

---
+++ src/components/StratagemList.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import StratagemSublist from "./StratagemSublist";
+
+function StratagemList() {
+    const name = 'Bridge'
+    return <div>
+        <StratagemSublist sectionName={name}></StratagemSublist>
+    </div>
+}
+
+export default StratagemList;
