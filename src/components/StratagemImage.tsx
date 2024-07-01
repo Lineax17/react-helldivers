511a7deb9ac406538d235a991da7161e167065b6
Lineax17 <hartmap04@gmail.com>
2024-07-01 22:52:37 +0200

    Creating StratagemPage.tsx

--- src/components/StratagemImage.tsx
+++ src/components/StratagemImage.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import StratagemPage from "./StratagemPage";
 
 interface StratagemImageProps {
   imagePath: string;
 }
 
 const StratagemImage: React.FC<StratagemImageProps> = ({ imagePath }) => {
-  return <div className="stratagem_image" onClick={() => console.log("Clicked. Placeholder for function")}><img src={imagePath} alt="Stratagem Image" /></div>;
+  return <div className="stratagem_image" onClick={() =>"Onclick Image Test"}><img src={imagePath} alt="Stratagem Image" /></div>;
 };
 
 export default StratagemImage;
